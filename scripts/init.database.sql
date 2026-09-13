2e12aa1a677616239eb4fbdca03f16263f49a4c9
mastermadhav04 <madhav221204@gmail.com>
2026-09-13 21:32:09 +0530

    Enhance comments in init.database.sql script
    Updated comments for clarity and added explanations for SQL commands.

--- scripts/init.database.sql
+++ scripts/init.database.sql
@@ -1,7 +1,7 @@
 /*
-===========================================================
+===========================================================================================
 Create Database and Schemas
-===========================================================
+===========================================================================================
 Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists.
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
@@ -11,14 +11,16 @@ Warning:
     Running this script will drop the entire 'DataWarehouse' database if it exists.
     All data inside the database will be permanently deleted. Proceed with caution and ensure we have
     proper backups before running this script.
+===========================================================================================
 */
 
-USE master; -- master database is the main control center of a SQL server instance (server directory)
+USE master;
+-- master database is the main control center of a SQL server instance (server directory of DBs)
 GO 
 ---- 'GO' is a SQL separator, which is telling SQL Server to firstly excecute the above command, 
 ---- then executing the next command.
 
--- Drop and recreate the 'DataWarehouse' database
+-- Dropping and recreating the 'DataWarehouse' database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
@@ -28,14 +30,14 @@ BEGIN
 END;
 GO
   
--- Create the 'DataWarehouse' database
+-- Creating the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
 GO 
 
 USE DataWarehouse;
 GO 
 
--- Create Schemas 
+-- Creating Schemas 
 CREATE SCHEMA bronze;
 GO
   
